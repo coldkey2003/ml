7c91ae15777521f6a5fe739ff9785108c83b94b0
zhao.jia <zhao.jia@immomo.com>
2018-01-31 21:22:11 +0800

    20180131 21:22

--- classification/trees/decision_tree/tree.py
+++ classification/trees/decision_tree/tree.py
@@ -17,3 +17,32 @@ array([[ 0.,  1.]])
 
 '''
 
+class Options(object):
+    def __init__(self):
+        self.criterion = 'gini'
+        self.max_depth = '8'
+        self.min_samples_leaf = 2
+        self.max_leaf_nodes = 100
+
+class tree(object):
+    def __init__(self, config):
+        self.criterion = config.criterion
+        self.max_depth = config.max_depth
+        self.min_samples_leaf = config.min_samples_leaf
+        self.max_leaf_nodes = config.max_leaf_nodes
+
+    #TODO
+    def gini_index(self, attr, label):
+        return 0
+
+    def get_attr_importance_value(self, metric_type, attr, label):
+        if metric_type == 'gini':
+            return self.gini_index(attr, label)
+
+    #TODO
+    def choose_best_attr(self, attr_list, label):
+        pass
+
+    #TODO
+    def build_tree(self):
+        pass
